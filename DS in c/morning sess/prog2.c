1250b62cb960e5c65d6f1496afc6710bc8e9cc24
Shubham0013 <shubhamsalekar0013@gmail.com>
2022-12-16 16:19:44 +0530

    morning sess

---
+++ DS in c/morning sess/prog2.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    struct node *prev;
+    int data;
+    struct node *next;
+
+} node;
+node *head = NULL;
+
+node *createnode(){
+
+    node *newnode = (node*)malloc(sizeof(node));
+
+    newnode->prev = NULL;
+
+    printf("Enter data\n");
+    scanf("%d",&newnode->data);
+
+    newnode->next = NULL;
+
+    return newnode;
+}
+void addnode(){
+
+        node *newnode= createnode();
+
+        if(head == NULL){
+            head = newnode;
+
+        }else{
+            node *temp = head;
+            while(temp->next != NULL){
+
+                temp = temp->next;
+            }
+            temp->next = newnode;
+        }
+}
+int printll(){
+
+    if(head == NULL){
+
+        printf("LL is empty\n");
+        return -1;
+    }else{
+
+        node *temp = head;
+    
+        while(temp->next != NULL){
+
+            printf("|%d|->",temp->data);
+            temp = temp->next;
+        }
+        printf("|%d|\n",temp->data);
+
+        return 0;
+    }
+}
+int inplaceR(){
+
+    if(head == NULL){
+        printf("LL is empty\n");
+        return -1;
+    }else if(head->next == NULL){
+
+            printf("Only one node is present i.e. %d \n",head->data);
+    }else{
+
+            node *temp = NULL;
+            while(head != NULL){
+                
+                head->prev = head->next;
+                head->next = temp;
+
+                if(head->prev == NULL){
+                    head = temp->prev;
+                    break;
+                }else{
+
+                    head = head->prev;
+                    temp = head->prev;
+                }
+            }
+        }
+	return 0;
+    }
+
+void main(){
+
+    char choice;
+
+    do
+    {
+
+        printf("1.addnode\n");
+        printf("2.printll\n");
+        printf("3.INPLACE raverse\n");
+
+        int ch;
+printf("enter choice\n");
+        scanf("%d", &ch);
+
+        switch (ch){
+            case 1:{
+
+                int n;
+                printf("enter node count\n");
+                scanf("%d",&n);
+
+                for(int i=1 ; i<=n ; i++){
+                    addnode();
+                }
+            }
+                 break;
+            case 2:
+                printll();
+                break;
+            case 3:
+                inplaceR();
+                break;
+            default:
+                printf("unknown choice\n");
+                break;
+        }
+        getchar();
+        printf("do you want to contonue\n");
+        scanf("%c",&choice);
+
+    }while(choice == 'Y' || choice == 'y');
+}
---
+++ DS in c/morning sess/prog2a.c
@@ -0,0 +1,121 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef struct node{
+
+	struct node *prev;
+	int data;
+	struct node *next;
+}node;
+
+node *head = NULL;
+
+node* createnode(){
+
+	node *newnode = (node*)malloc(sizeof(node));
+
+	newnode->prev = NULL;
+
+	printf("Enter data\n");
+	scanf("%d",&newnode->data);
+
+	newnode->next = NULL;
+
+	return newnode;
+}
+void addnode(){
+
+	node *newnode = createnode();
+
+	if(head == NULL){
+	
+		head = newnode;
+	}else{
+	
+		node *temp = head;
+		while(temp->next !=0){
+		
+			temp = temp->next;
+		}
+		temp->next = newnode;
+		newnode ->prev = temp;
+	}
+}
+void printll(){
+
+	node *temp = head;
+	
+	while(temp!=0){
+	
+		if(head = 0){
+		
+			printf("| %d |",temp->data);
+		}else{
+		
+			printf("| %d |->",temp->data);
+		}
+	
+		temp=temp->next;
+	}
+
+}
+void inplaceR(){
+
+	node *temp = NULL;
+
+	while(head != NULL){
+
+		temp = head->prev;
+		head->prev = head->next;
+	    head->next = temp;
+		head = head->prev;
+    
+	}
+	head = temp->prev;
+}
+void main(){
+
+	char choice;
+
+	do{
+	
+		printf("1.add node\n");
+		printf("2.Show total nodes\n");
+		printf("3.inplace reverse\n");
+		
+		int ch;
+		printf("Enter choice\n");
+		scanf("%d",&ch);
+
+		switch(ch){
+		
+			case 1:{
+				int n;
+				printf("enter node count\n");
+				scanf("%d",&n);
+
+				for (int i=1 ; i<=n ; i++){
+				
+					addnode();
+				}
+			       }
+				break;
+			case 2:
+				printll();
+				break;
+			case 3:
+				inplaceR();
+				break;
+			default:
+				printf("Wrong choice\n");
+
+		}
+		getchar();
+		printf("Do you to continue\n");
+		scanf("%c",&choice);
+	
+	}while(choice == 'y' || choice == 'Y' );
+
+
+}
